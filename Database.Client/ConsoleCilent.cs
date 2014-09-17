3081dafe10c0f6ba71fd792806eddfb89763e38b
stoianpp <stoianpp2013@gmail.com>
2014-09-17 14:55:32 +0300

    Small Fixes

--- Database.Client/ConsoleCilent.cs
+++ Database.Client/ConsoleCilent.cs
@@ -17,19 +17,16 @@
             using (db)
             {
                 IUserRepository usersAdding = new UserRepository(db);
-                var users = new List<User>();
                 var fileContent = File.ReadAllText(@"..\..\Users.json");
                 var currentUsers = JsonConvert.DeserializeObject<IEnumerable<User>>(fileContent);
                 usersAdding.InsertAll(currentUsers);
 
                 IOpenQuestionRepository openQuestionsAdding = new OpenQuestionRepository(db);
-                var openQuestions = new List<OpenQuestion>();
                 var fileContentOpenQuestions = File.ReadAllText(@"..\..\OpenQuestions.json");
                 var currentOpenQuestions = JsonConvert.DeserializeObject<IEnumerable<OpenQuestion>>(fileContentOpenQuestions);
                 openQuestionsAdding.InsertAll(currentOpenQuestions);
 
                 IClosedQuestionRepository closedQuestionsAdding = new ClosedQuestionRepository(db);
-                var closedQuestions = new List<ClosedQuestion>();
                 var fileContentClosedQuestions = File.ReadAllText(@"..\..\ClosedQuestions.json");
                 var currentClosedQuestions = JsonConvert.DeserializeObject<IEnumerable<ClosedQuestion>>(fileContentClosedQuestions);
                 closedQuestionsAdding.InsertAll(currentClosedQuestions);
