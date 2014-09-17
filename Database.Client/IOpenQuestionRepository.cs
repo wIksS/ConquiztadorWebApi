18bd574478f4cae87d6abb9cdfefae09a23e11f7
stoianpp <stoianpp2013@gmail.com>
2014-09-17 14:52:14 +0300

    JSON files added

---
+++ Database.Client/IOpenQuestionRepository.cs
@@ -0,0 +1,22 @@
+﻿namespace GameDb.Client
+{
+    using System.Collections.Generic;
+    using System.Linq;
+
+    using GameDb.Models;
+
+    public interface IOpenQuestionRepository : IRepository
+    {
+        IQueryable All{ get; }
+
+        void Insert(string name);
+
+        void InsertAll(IEnumerable<OpenQuestion> questions);
+
+        bool UpdateAnswers(string question, int answer);
+
+        bool Delete(string name);
+
+        void Save();
+    }
+}
